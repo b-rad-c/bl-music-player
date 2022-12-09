865b3abf44ca9c172461232ac6007481971f6348
b-rad-c <bradcorlett@gmail.com>
2022-12-08 23:26:57 -0800

    add visualizer params to starup.blend

--- bl_music_player/addons/music_player/ops.py
+++ bl_music_player/addons/music_player/ops.py
@@ -20,12 +20,13 @@
 
 from pathlib import Path
 from typing import Optional, List, Callable, Set
+from random import randint
 
 import bpy
-import aud
 
 from bpy.app.handlers import persistent
 from music_player import opsdata, config
+
 
 
 #
@@ -33,23 +34,56 @@ from music_player import opsdata, config
 #
 
 
-# active_media_area: str = 'SEQUENCE_EDITOR'
-# active_media_area_obj: bpy.types.Area = None
-
 active_directory: Path = ''
 active_filename: Optional[str] = ''
 previous_directory: Path = ''
 previous_filename: Optional[str] = ''
 
-play_audio: bool = True
-
-screen_index: int = 1
-
+play_audio = True
+screen_index = 1
+is_fullscreen = False
+random_vis_on_play = True
 
 
 #
 # operators
 #
+
+
+@persistent
+def randomize_vis():
+    print('\trandomize_vis()')
+
+    waveform = bpy.context.active_object
+    waveform['color_preset'] = randint(0, 3)
+
+    nodes = waveform.modifiers['GeometryNodes']
+    nodes['Input_2'] = bool(randint(0, 1))
+    nodes['Input_3'] = bool(randint(0, 1))
+
+    # bpy.context.scene.update_render_engine()
+    # bpy.context.scene.update_tag()
+    bpy.context.view_layer.update()
+
+
+class MP_OP_randomize_visualizer(bpy.types.Operator):
+
+    bl_idname = 'music_player.randomize_visualizer'
+    bl_label = 'Randomize visualizer'
+    bl_description = 'Sets random values for the visualizer parameters'
+
+    def execute(self, context) -> Set[str]:
+        waveform = context.active_object
+        waveform['color_preset'] = randint(0, 3)
+
+        nodes = waveform.modifiers['GeometryNodes']
+        nodes['Input_2'] = bool(randint(0, 1))
+        nodes['Input_3'] = bool(randint(0, 1))
+
+        # bpy.context.scene.update_render_engine()
+        # bpy.context.scene.update_tag()
+        context.view_layer.update()
+        return {'FINISHED'}
 
 
 class MP_OP_play(bpy.types.Operator):
@@ -61,6 +95,8 @@ class MP_OP_play(bpy.types.Operator):
     sound_path: bpy.props.StringProperty(name='Sound path', description='The path to a sound file to play')
 
     def execute(self, context) -> Set[str]:
+        global random_vis_on_play
+
         print(f'music_player.play({self.sound_path})')
 
         # reset sequence
@@ -85,10 +121,15 @@ class MP_OP_play(bpy.types.Operator):
         graph_context = opsdata.get_context_for_area(graph_area)
         bpy.ops.graph.sound_bake(graph_context, filepath=self.sound_path)
 
+        if random_vis_on_play:
+            print('\trandomizing...')
+            bpy.ops.music_player.randomize_visualizer()
+
         print('\tplaying...')
 
         bpy.ops.screen.animation_play(sync=True)
         print('\tdone.')
+
         return {'FINISHED'}
 
 
@@ -110,13 +151,24 @@ class MV_OT_fullscreen(bpy.types.Operator):
 
 
     def execute(self, context: bpy.types.Context) -> Set[str]:
+        global is_fullscreen
+
         view_3d_area = opsdata.find_area(context, 'VIEW_3D')
         view_3d_context = opsdata.get_context_for_area(view_3d_area)
 
         with bpy.context.temp_override(**view_3d_context):
-            bpy.ops.screen.header_toggle_menus()
-            bpy.ops.screen.screen_full_area(use_hide_panels=True)
-            bpy.ops.wm.window_fullscreen_toggle()
+            #bpy.ops.wm.window_fullscreen_toggle()
+            #bpy.ops.screen.header_toggle_menus()
+
+            if is_fullscreen:
+                bpy.ops.screen.back_to_previous()
+            else:
+                bpy.ops.screen.header_toggle_menus()
+                bpy.ops.screen.screen_full_area(use_hide_panels=True)
+                #bpy.context.window.workspace = bpy.data.workspaces['APP_CLEAN']
+                bpy.ops.wm.window_fullscreen_toggle()
+
+            is_fullscreen = not is_fullscreen
         
         return {'FINISHED'}
 
@@ -135,13 +187,6 @@ def init_3d_viewport(_):
                     space.show_gizmo = False
                     space.overlay.show_overlays = False
 
-                    # space.overlay.wireframe_opacity = 0.0
-                    # space.show_object_viewport_lattice = False
-                    # space.show_object_viewport_meta = False
-                    # sp = space
-                    # ov = space.overlay
-                    # breakpoint()
-
     print('\t-> done')
 
 @persistent
@@ -151,7 +196,6 @@ def init_filebrowser(_):
     params.display_type = 'LIST_VERTICAL'
     print('\t-> done')
 
-
 @persistent
 def callback_filename_change(_):
     # init globals
@@ -160,7 +204,6 @@ def callback_filename_change(_):
     global previous_directory
     global previous_filename
     global play_audio
-    global audio_device
     
 
     # update globals
@@ -171,13 +214,11 @@ def callback_filename_change(_):
 
     # check active file
     if bpy.context.active_file:
-        # print('file active')
         active_filename = bpy.context.active_file.relative_path
         if active_filename != previous_filename:
             audio_path = active_directory / active_filename
 
             if opsdata.is_audio(audio_path):
-                # print(f'new audio: {audio_path=}')
                 if play_audio:
                     bpy.ops.music_player.stop()
                     bpy.ops.music_player.play(sound_path=audio_path.as_posix())
@@ -193,7 +234,7 @@ def callback_filename_change(_):
 # register
 #
 
-classes = [MP_OP_play, MP_OP_stop, MV_OT_fullscreen]
+classes = [MP_OP_randomize_visualizer, MP_OP_play, MP_OP_stop, MV_OT_fullscreen]
 load_post_handlers = [init_3d_viewport, init_filebrowser]
 draw_handlers_fb: List[Callable] = []
 
