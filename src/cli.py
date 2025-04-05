0ca8d2ebe35b3d7717dcbdbcbb4e74ee6c5610d7
b rad c <bradcorlett@gmail.com>
2025-04-04 17:21:26 -0700

    begin implement threading

--- src/cli.py
+++ src/cli.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-import bpy
+# import bpy
 import sys
 import os
 import time
 import argparse
+import multiprocessing
 from contextlib import contextmanager
 from pathlib import Path
-from music_player.opsdata import load_and_bake_audio
 
 
 # suprress stdout during render (ref: https://blender.stackexchange.com/a/270201/156811)
@@ -40,34 +40,62 @@ src_dir = Path(__file__).parent
 blend_file = src_dir / 'bl_music_player_app/startup.blend'
 default_audio_file = src_dir / 'music_player' / 'music' / 'Ketsa - You Best Boogie.mp3'
 
-# cli #
+def render_thread(threads:int, thread_num:int, audio:Path, output:Path):
+    import bpy
+    from music_player.opsdata import load_and_bake_audio
 
-parser = argparse.ArgumentParser(description='Render a musical animation.')
-parser.add_argument('--audio', '-a', type=str, help='Path to the audio file', default=str(default_audio_file))
-parser.add_argument('--output', '-o', type=str, help='Path to the output file', default='output.mp4')
+    bpy.ops.wm.open_mainfile(filepath=str(blend_file))
+    load_and_bake_audio(bpy.context, sound_path=str(audio.absolute()), background=True)
 
-args = parser.parse_args()
+    output = output.with_suffix(f'.{thread_num}.mp4')
+    bpy.context.scene.render.filepath = str(output.absolute())
 
-if not os.path.exists(args.audio):
-    raise FileNotFoundError(f'Audio file not found: {args.audio}')
+    print('rendering: ', bpy.context.scene.render.filepath)
 
-if not args.output.endswith('.mp4'):
-    raise ValueError('Output file must be an .mp4 file')
+    bpy.context.scene.frame_start = thread_num
+    bpy.context.scene.frame_step = threads
+    with stdout_redirected():
+        bpy.ops.render.render(animation=True)
 
-# load and bake audio #
 
-bpy.ops.wm.open_mainfile(filepath=str(blend_file))
-load_and_bake_audio(bpy.context, sound_path=os.path.abspath(args.audio), background=True)
+def main(threads:int, audio:Path, output:Path):
 
-# render #
+    process_args = [(threads, i, audio, output) for i in range(1, threads + 1)]
 
-bpy.context.scene.render.filepath = args.output
+    with multiprocessing.Pool(processes=threads) as pool:
+        start = time.time()
 
-with stdout_redirected():
-    start = time.time()
-    bpy.ops.render.render(animation=True)
-    end = time.time()
+        result = pool.starmap_async(render_thread, process_args)
+        pool.close()
+        pool.join()
 
-total_seconds = end - start
-print('render time:', round(total_seconds / 60, 2), 'mins')
-print('done.')
+        end = time.time()
+
+    print(type(result), result)
+
+    print(result.successful())
+    print(result.get())
+
+    total_seconds = end - start
+    print('render time:', round(total_seconds / 60, 2), 'mins')
+    print('done.')
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+
+    # cli #
+
+    parser = argparse.ArgumentParser(description='Render a musical animation.')
+    parser.add_argument('--audio', '-a', type=Path, help='Path to the audio file', default=str(default_audio_file))
+    parser.add_argument('--output', '-o', type=Path, help='Path to the output file', default='output.mp4')
+    parser.add_argument('--threads', '-t', type=int, help='Number of threads to use (default=8)', default=8)
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.audio):
+        raise FileNotFoundError(f'Audio file not found: {args.audio}')
+
+    if args.output.suffix != '.mp4':
+        raise ValueError('Output file must be an .mp4 file')
+    
+    main(args.threads, args.audio, args.output)
